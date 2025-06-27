05542760df9dde5d69a44b087ace9f478eae6f8b
SaiChuquipiondo <saichuquipiondopinchi@gmail.com>
2025-06-26 20:54:38 -0500

    Commit incial

---
+++ TrenSistema/src/main/java/com/tren/demo/TrenSistemaApplication.java
@@ -0,0 +1,13 @@
+package com.tren.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrenSistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrenSistemaApplication.class, args);
+	}
+
+}
---
+++ TrenSistema/src/test/java/com/tren/demo/TrenSistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tren.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrenSistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
